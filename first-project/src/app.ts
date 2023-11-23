f3b00b52cbdac71df7d1406215260bf03a5c2c29
mdsumonali372 <mdsumonali026@gmail.com>
2023-11-23 17:20:30 +0600

    project updated get data find data

--- first-project/src/app.ts
+++ first-project/src/app.ts
@@ -1,10 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 // perser
 app.use(express.json());
 app.use(cors());
+
+// application
+
+app.use('/api/v1/students', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   const a = 20;
---
+++ first-project/src/app/modules/student/student.models.ts
@@ -0,0 +1,48 @@
+import { Schema, model } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student.interface';
+
+const userSchema = new Schema<UserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String },
+  motherContactNo: { type: String, required: true },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String },
+  contactNo: { type: String, required: true },
+  address: { type: String },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userSchema,
+  gender: ['male', 'female', 'other'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloogGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'blocked'],
+});
+
+export const StudentModel = model<Student>('Student', studentSchema);
